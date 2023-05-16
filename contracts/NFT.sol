b817b0cd62a17aaa1669ae1243911c529f0fe159
Michael Longoria <michaelandrewlongoria@gmail.com>
2023-05-16 00:13:37 -0700

    Add Minting functions

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -5,10 +5,15 @@ import "./ERC721Enumerable.sol";
 import "./Ownable.sol";
 
 contract NFT is ERC721Enumerable, Ownable {
+    using Strings for uint256;
+
+    string public baseURI;
+    string public baseExtension = '.json';
     uint256 public cost;
     uint256 public maxSupply;
     uint256 public allowMintingOn;
-    string public baseURI;
+
+    event Mint(uint256 amount, address minter);
 
     constructor(
         string memory _name,
@@ -24,4 +29,43 @@ contract NFT is ERC721Enumerable, Ownable {
         baseURI = _baseURI;
     }
 
+    function mint(uint256 _mintAmount) public payable {
+        require(block.timestamp >= allowMintingOn, "Minting not allowed yet");
+        require(_mintAmount > 0, "Must mint at least 1 token");
+        require(msg.value >= cost * _mintAmount, "Not enough payment");
+
+        uint256 supply = totalSupply();
+        require(supply + _mintAmount <= maxSupply, "Not enough tokens remaining");
+
+        // Create tokens
+        for(uint256 i = 1; i <= _mintAmount; i++) {
+            _safeMint(msg.sender, supply + i);
+        }
+
+        // Emit event
+        emit Mint(_mintAmount, msg.sender);
+    }
+
+    // Return metadata IPFS url
+    // e.g. 'ipfs://QmQ2jnDYecFhrf3asEWjyjZRX1pZSsNWG3qHzmNDvXa9qg/1.json'
+    function tokenURI(uint256 _tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        require(_exists(_tokenId), 'Token does not exist');
+        return(string(abi.encodePacked(baseURI, _tokenId.toString(), baseExtension)));
+    }
+
+    function walletOfOwner(address _owner) public view returns(uint256[] memory) {
+        uint256 ownerTokenCount = balanceOf(_owner);
+        uint256[] memory tokenIds = new uint256[](ownerTokenCount);
+
+        for(uint256 i; i < ownerTokenCount; i++) {
+            tokenIds[i] = tokenOfOwnerByIndex(_owner, i);
+        }
+        return tokenIds;
+    }
 }
