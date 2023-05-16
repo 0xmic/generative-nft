72b1aa820da5743ee1b95a26056563e5aeda472b
Michael Longoria <michaelandrewlongoria@gmail.com>
2023-05-16 09:40:25 -0700

    Add Withdraw and setCost functions

--- test/NFT.js
+++ test/NFT.js
@@ -168,4 +168,77 @@ describe('NFT', () => {
       expect(tokenIds[2].toString()).to.equal('3')
     })
   })
+
+  describe('Withdrawing ETH', () => {
+    let transaction, result, balanceBefore
+
+    describe('Success', async () => {
+      const ALLOW_MINTING_ON = Date.now().toString().slice(0, 10) // Now
+
+      beforeEach(async () => {
+        const NFT = await ethers.getContractFactory('NFT')
+        nft = await NFT.deploy(NAME, SYMBOL, COST, MAX_SUPPLY, ALLOW_MINTING_ON, BASE_URI)
+
+        transaction = await nft.connect(minter).mint(1, { value: COST })
+        result = await transaction.wait()
+
+        balanceBefore = await ethers.provider.getBalance(deployer.address)
+
+        transaction = await nft.connect(deployer).withdraw()
+        result = await transaction.wait()
+      })
+
+      it('deducts contract balance', async () => {
+        expect(await ethers.provider.getBalance(nft.address)).to.equal(0)
+      })
+
+      it('sends funds to the owner', async () => {
+        expect(await ethers.provider.getBalance(deployer.address)).to.be.greaterThan(balanceBefore)
+      })
+
+      it('emits a Withdraw event', async () => {
+        expect(transaction).to.emit(nft, 'Withdraw').withArgs(COST, deployer.address)
+      })
+    })
+
+    describe('Failure', async () => {
+      it('prevents non-owner from withdrawing', async () => {
+        const ALLOW_MINTING_ON = Date.now().toString().slice(0, 10) // now
+        const NFT = await ethers.getContractFactory('NFT')
+        nft = await NFT.deploy(NAME, SYMBOL, COST, MAX_SUPPLY, ALLOW_MINTING_ON, BASE_URI)
+
+        await expect(nft.connect(minter).withdraw()).to.be.reverted
+      })
+    })
+  })
+
+  describe('Updating Cost', () => {
+    let transaction, result, balanceBefore
+
+    describe('Success', async () => {
+      const ALLOW_MINTING_ON = Date.now().toString().slice(0, 10) // Now
+
+      beforeEach(async () => {
+        const NFT = await ethers.getContractFactory('NFT')
+        nft = await NFT.deploy(NAME, SYMBOL, COST, MAX_SUPPLY, ALLOW_MINTING_ON, BASE_URI)
+      })
+
+      it('updates cost', async () => {
+        let newCost = ether(100)
+        await nft.connect(deployer).setCost(newCost)
+        expect(await nft.cost()).to.equal(newCost)
+      })
+    })
+
+    describe('Failure', async () => {
+      it('prevents non-owner from updating cost', async () => {
+        const ALLOW_MINTING_ON = Date.now().toString().slice(0, 10) // now
+        const NFT = await ethers.getContractFactory('NFT')
+        nft = await NFT.deploy(NAME, SYMBOL, COST, MAX_SUPPLY, ALLOW_MINTING_ON, BASE_URI)
+
+        let newCost = ether(100)
+        await expect(nft.connect(minter).setCost(newCost)).to.be.reverted
+      })
+    })
+  })
 })
